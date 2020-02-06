98136c6848f3e732cc48e18077b7c9e1cb6229b5
william moreschi <williammoreschi@gmail.com>
2020-02-05 23:42:01 -0300

    Atualizando os dados do usuário. Inclusive a senha

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,7 +1,6 @@
 import User from '../models/User';
 
 class UserController {
-  // eslint-disable-next-line consistent-return
   async store(req, res) {
     const userExists = await User.findOne({ where: { email: req.body.email } });
 
@@ -10,12 +9,27 @@ class UserController {
     }
 
     const { id, name, email, provider } = await User.create(req.body);
-    res.json({ id, name, email, provider });
+    return res.json({ id, name, email, provider });
   }
 
   async update(req, res) {
-    // console.log(req.userId);
-    return res.json('xxx');
+    const { email, oldPassword } = req.body;
+    const user = await User.findByPk(req.userId);
+    if (email && email !== user.email) {
+      const userExists = await User.findOne({ where: { email } });
+
+      if (userExists) {
+        return res.status(400).json({ error: 'User already exists' });
+      }
+    }
+
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(401).json({ error: 'Password does not match' });
+    }
+
+    const { id, name, provider } = await user.update(req.body);
+
+    return res.json({ id, name, email, provider });
   }
 }
 
