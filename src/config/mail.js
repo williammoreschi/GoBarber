8b58f563deef1854639f5e13a67b19416b0c29be
william moreschi <williammoreschi@gmail.com>
2020-02-12 20:44:10 -0300

    Criado a class de envio de email

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '17f56d05311940',
+    pass: '6238dc7e80e6a2',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
