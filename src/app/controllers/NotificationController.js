9929f61b0f5ba0c513f7a820eac24f3ee67c22b1
william moreschi <williammoreschi@gmail.com>
2020-02-12 19:54:45 -0300

    Validando se notificação existe e se pode ser altearda

--- src/app/controllers/NotificationController.js
+++ src/app/controllers/NotificationController.js
@@ -24,6 +24,18 @@ class NotificationController {
   }
 
   async update(req, res) {
+    const checkNotifcation = await Notification.findById(req.params.id);
+
+    if (!checkNotifcation) {
+      return res.status(400).json({ error: 'Notification not found' });
+    }
+
+    if (checkNotifcation.user !== req.userId) {
+      return res.status(401).json({
+        error:
+          'You do not is permitted change the notification from other user',
+      });
+    }
     /**
      * A opcao new:true depois de atalizar o registro
      * ele vai devolver o mesmo atualizado
