e175eddc60accba3bbfa5133408af25ec747dd33
william moreschi <williammoreschi@gmail.com>
2020-02-13 08:31:18 -0300

    Configurando o envio de email usando o Redis Usamos o Bequeue para conectar no container do redis criado usando docker; Pra não rodar o bequeue junto a aplicação foi criado um sript que vai rodar ele de forma independente 'yarn queue';

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -6,7 +6,8 @@ import File from '../models/File';
 import Appointment from '../models/Appointment';
 import Notification from '../schemas/Notification';
 
-import Mail from '../../lib/Mail';
+import CancellationMail from '../jobs/CancellationMail';
+import Queue from '../../lib/Queue';
 
 class AppointmentController {
   async index(req, res) {
@@ -151,22 +152,7 @@ class AppointmentController {
 
     await appointment.save();
 
-    const formattedDate = format(
-      appointment.date,
-      "'dia' dd 'de' MMMM', às ' H:mm'h'",
-      { locale: pt }
-    );
-
-    await Mail.sendMail({
-      to: `${appointment.provider.name} <${appointment.provider.email}>`,
-      subject: 'Agendamento Cancelado',
-      template: 'cancellation',
-      context: {
-        provider: appointment.provider.name,
-        user: appointment.user.name,
-        date: formattedDate,
-      },
-    });
+    await Queue.add(CancellationMail.key, { appointment });
 
     return res.json(appointment);
   }
