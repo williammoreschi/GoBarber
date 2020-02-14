0923ebfdd0003912b7e52e8bcc8cd28a73d9930d
william moreschi <williammoreschi@gmail.com>
2020-02-14 07:39:10 -0300

    Sentry foi adiconado junto a aplicação para monitorarmos os erros que podem acontecer

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import path from 'path';
+import * as Sentry from '@sentry/node';
 import routes from './routes';
+import 'express-async-errors';
+import sentryConfig from './config/sentry';
 
 import './database/index';
 
@@ -8,11 +11,14 @@ class App {
   constructor() {
     this.server = express();
 
+    Sentry.init(sentryConfig);
+
     this.middlewares();
     this.routes();
   }
 
   middlewares() {
+    this.server.use(Sentry.Handlers.requestHandler());
     this.server.use(express.json());
     this.server.use(
       '/files',
@@ -22,6 +28,7 @@ class App {
 
   routes() {
     this.server.use(routes);
+    this.server.use(Sentry.Handlers.errorHandler());
   }
 }
 
