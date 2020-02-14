494005895cfaf4fc8694301fa8a221396d0b158c
william moreschi <williammoreschi@gmail.com>
2020-02-14 06:44:59 -0300

    Criado um campo 'past' virtual pra mostar se o agendamento ja passou do horário

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -17,7 +17,7 @@ class AppointmentController {
         user_id: req.userId,
         canceled_at: null,
       },
-      attributes: ['id', 'date'],
+      attributes: ['id', 'date', 'past'],
       limit: 20,
       offset: (page - 1) * 20,
       order: ['date'],
