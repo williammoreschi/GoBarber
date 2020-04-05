9a8e76431bb2d86e349b14e767f6f27c2e259338
william moreschi <williammoreschi@gmail.com>
2020-04-05 19:10:22 -0300

    Style Global

---
+++ frontend/src/styles/global.js
@@ -0,0 +1,39 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap');
+*{
+  margin: 0;
+  padding: 0;
+  outline: 0;
+  box-sizing: border-box;
+
+}
+
+*:focus{
+  outline: 0;
+}
+
+html, body, #root{
+  height:100%;
+}
+
+body{
+  -webkit-font-smoothing: antialiased !important;
+}
+
+body, input, button{
+  font-family: 'Roboto', sans-serif;
+}
+
+a{
+  text-decoration:none;
+}
+
+ul,ol{
+  list-style:none
+}
+button{
+  cursor:pointer;
+}
+`;
