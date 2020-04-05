148db1b08c8e7d36ed2b9a8f0b11de2fada48023
william moreschi <williammoreschi@gmail.com>
2020-04-05 16:34:17 -0300

    Criando o esquema de rotas privadas

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -1,8 +1,10 @@
 import React from "react";
-import { Switch, Route } from "react-router-dom";
+import { Switch } from "react-router-dom";
+import Route from "./Route";
 
 import SingIn from "../pages/SingIn";
 import SingUp from "../pages/SingUp";
+
 import Dashboard from "../pages/Dashboard";
 import Profile from "../pages/Profile";
 
@@ -11,8 +13,11 @@ export default function Routes() {
     <Switch>
       <Route path="/" exact component={SingIn} />
       <Route path="/register" component={SingUp} />
-      <Route path="/dashboard" component={Dashboard} />
-      <Route path="/profile" component={Profile} />
+
+      <Route path="/dashboard" component={Dashboard} isPrivate />
+      <Route path="/profile" component={Profile} isPrivate />
+
+      <Route path="/" component={() => <h1>404</h1>} />
     </Switch>
   );
 }
