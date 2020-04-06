ec9e24629d74989c054f4fe2b5437fb368832421
william moreschi <williammoreschi@gmail.com>
2020-04-06 14:13:57 -0300

    Utilizando a lib Unform da Rocketseat

--- frontend/src/pages/SingIn/index.js
+++ frontend/src/pages/SingIn/index.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import { Form, Input } from '@rocketseat/unform';
 import logo from '~/assets/images/logo.svg';
 
 export default function SingIn() {
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
   return (
     <>
       <img src={logo} alt="GoBaber" />
-      <form>
-        <input type="email" placeholder="Seu e-mail" />
-        <input type="password" placeholder="Sua senha secreta" />
+      <Form onSubmit={handleSubmit}>
+        <Input name="email" type="email" placeholder="Seu e-mail" />
+        <Input
+          name="password"
+          type="password"
+          placeholder="Sua senha secreta"
+        />
         <button type="submit">Acessar</button>
         <Link to="/register">Criar conta gratuita</Link>
-      </form>
+      </Form>
     </>
   );
 }
--- frontend/src/pages/SingUp/index.js
+++ frontend/src/pages/SingUp/index.js
@@ -1,19 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import { Form, Input } from '@rocketseat/unform';
 import logo from '~/assets/images/logo.svg';
 
 export default function SingUp() {
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
   return (
     <>
       <img src={logo} alt="GoBaber" />
-      <form>
-        <input placeholder="Nome Completo" />
-        <input type="email" placeholder="Seu e-mail" />
-        <input type="password" placeholder="Sua senha secreta" />
+      <Form onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Nome Completo" />
+        <Input name="email" type="email" placeholder="Seu e-mail" />
+        <Input
+          name="password"
+          type="password"
+          placeholder="Sua senha secreta"
+        />
         <button type="submit">Criar conta</button>
         <Link to="/">Já tenho login</Link>
-      </form>
+      </Form>
     </>
   );
 }
