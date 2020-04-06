ed4e3090041f889dc54eb0f1d7e75890db454e12
william moreschi <williammoreschi@gmail.com>
2020-04-06 14:01:59 -0300

    Criado a estilização das telas de autenticação

--- frontend/src/pages/SingIn/index.js
+++ frontend/src/pages/SingIn/index.js
@@ -1,7 +1,18 @@
-import React from "react";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-// import { Container } from './styles';
+import logo from '~/assets/images/logo.svg';
 
 export default function SingIn() {
-  return <h1>Login</h1>;
+  return (
+    <>
+      <img src={logo} alt="GoBaber" />
+      <form>
+        <input type="email" placeholder="Seu e-mail" />
+        <input type="password" placeholder="Sua senha secreta" />
+        <button type="submit">Acessar</button>
+        <Link to="/register">Criar conta gratuita</Link>
+      </form>
+    </>
+  );
 }
--- frontend/src/pages/SingUp/index.js
+++ frontend/src/pages/SingUp/index.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-// import { Container } from './styles';
+import logo from '~/assets/images/logo.svg';
 
 export default function SingUp() {
-  return <h1>Register</h1>;
+  return (
+    <>
+      <img src={logo} alt="GoBaber" />
+      <form>
+        <input placeholder="Nome Completo" />
+        <input type="email" placeholder="Seu e-mail" />
+        <input type="password" placeholder="Sua senha secreta" />
+        <button type="submit">Criar conta</button>
+        <Link to="/">Já tenho login</Link>
+      </form>
+    </>
+  );
 }
