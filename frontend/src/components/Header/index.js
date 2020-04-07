11c37ef7bd3f4b0952fd1aa60a5e4fe9cc3e714b
william moreschi <williammoreschi@gmail.com>
2020-04-07 10:50:22 -0300

    Criado o component Header da apicação

---
+++ frontend/src/components/Header/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Container, Content, Profile } from './styles';
+import logo from '~/assets/images/logo_small.svg';
+
+export default function Header() {
+  return (
+    <Container>
+      <Content>
+        <nav>
+          <img src={logo} alt="GoBarber" />
+          <Link to="/dashboard">DASHBOARD</Link>
+        </nav>
+        <aside>
+          <Profile>
+            <div>
+              <strong>William Moreschi</strong>
+              <Link to="/profile">Meu Perfil</Link>
+            </div>
+            <img
+              src="https://api.adorable.io/avatars/55/abott@adorable.png"
+              alt="William Moreschi"
+            />
+          </Profile>
+        </aside>
+      </Content>
+    </Container>
+  );
+}
