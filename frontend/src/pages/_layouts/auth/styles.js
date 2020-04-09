24606619a631364cf103eeef9843c9af884fb410
william moreschi <williammoreschi@gmail.com>
2020-04-09 11:16:27 -0300

    o backgrund nao acompanhava a pagina caso o conteudo fosse maior

--- frontend/src/pages/_layouts/auth/styles.js
+++ frontend/src/pages/_layouts/auth/styles.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 import { darken } from 'polished';
 
 export const Wrapper = styled.div`
-  height: 100%;
+  min-height: 100%;
   background: linear-gradient(-90deg, #7159c1, #ab59c1);
   display: flex;
   justify-content: center;
