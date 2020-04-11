e4cd31c01da2e5105a2c6234552a1a004ba242e1
william moreschi <williammoreschi@gmail.com>
2020-04-11 14:56:37 -0300

    Estilizado a tela de cadastro

--- mobile/src/pages/SingUp/index.js
+++ mobile/src/pages/SingUp/index.js
@@ -1,12 +1,56 @@
 import React from 'react';
-import {Text} from 'react-native';
-import Background from '~/components/Background';
-// import { Container } from './styles';
+import {Image} from 'react-native';
+import PropTypes from 'prop-types';
 
-export default function SingUp() {
+import logo from '~/assets/logo.png';
+
+import Background from '~/components/Background';
+
+import {
+  Container,
+  Form,
+  FormInput,
+  SubmitButton,
+  SingLink,
+  SingLinkText,
+} from './styles';
+
+export default function SingUp({navigation}) {
   return (
     <Background>
-      <Text>SingUp</Text>
+      <Container>
+        <Image source={logo} />
+        <Form>
+          <FormInput
+            icon="person-outline"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Nome Completo"
+          />
+          <FormInput
+            icon="mail-outline"
+            keyboardType="email-address"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Digite seu e-mail"
+          />
+          <FormInput
+            icon="lock-outline"
+            secureTextEntry
+            placeholder="Digite sua senha"
+          />
+          <SubmitButton onPress={() => {}}>Criar Conta</SubmitButton>
+        </Form>
+        <SingLink onPress={() => navigation.navigate('SingIn')}>
+          <SingLinkText>Já possuo uma conta </SingLinkText>
+        </SingLink>
+      </Container>
     </Background>
   );
 }
+
+SingUp.propTypes = {
+  navigation: PropTypes.shape({
+    navigate: PropTypes.func.isRequired,
+  }).isRequired,
+};
