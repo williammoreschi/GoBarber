7dd2a95615f8eb65e7dcf10fcfa1bc6502e202ae
william moreschi <williammoreschi@gmail.com>
2020-04-11 14:17:03 -0300

    Criado dois component globais o input e button

--- mobile/src/pages/SingIn/index.js
+++ mobile/src/pages/SingIn/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import {Text} from 'react-native';
+import Input from '~/components/Input';
+import Button from '~/components/Button';
 import Background from '~/components/Background';
 // import { Container } from './styles';
 
@@ -7,6 +9,8 @@ export default function SingIn() {
   return (
     <Background>
       <Text>SingIn</Text>
+      <Input icon="call" placeholder="Digite seu nome" />
+      <Button>Acessar</Button>
     </Background>
   );
 }
