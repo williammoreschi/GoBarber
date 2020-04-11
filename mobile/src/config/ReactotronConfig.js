2870159c266868146d77c68f49bfb1a689c39c45
william moreschi <williammoreschi@gmail.com>
2020-04-11 16:14:43 -0300

    Configurando o Redux Parte 3: Arrumando o async-storage para funcionar no mobile

--- mobile/src/config/ReactotronConfig.js
+++ mobile/src/config/ReactotronConfig.js
@@ -1,13 +1,15 @@
 import Reactotron from 'reactotron-react-native';
 import {reactotronRedux} from 'reactotron-redux';
 import reactotronSaga from 'reactotron-redux-saga';
+import AsyncStorage from '@react-native-community/async-storage';
 
 if (__DEV__) {
   /** Se no android ou no emulador nao aparecer nada tenta primeiro por
    * host: ip da maquina se não der usar o link abaixo
    * https://github.com/infinitered/reactotron/issues/162
    * */
-  const tron = Reactotron.configure({host: '192.168.15.11'})
+  const tron = Reactotron.setAsyncStorageHandler(AsyncStorage)
+    .configure({host: '192.168.15.11'})
     .useReactNative()
     .use(reactotronRedux())
     .use(reactotronSaga())
