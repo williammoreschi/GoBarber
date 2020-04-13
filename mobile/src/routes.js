2f5e0ef6fb1fd1b8f606d34459f06879e5336d6e
william moreschi <williammoreschi@gmail.com>
2020-04-13 17:57:16 -0300

    Criando a rota para quando o usuario fizer o login ir para a Dashboard

--- mobile/src/routes.js
+++ mobile/src/routes.js
@@ -1,11 +1,24 @@
 import {createAppContainer, createSwitchNavigator} from 'react-navigation';
-
+import {createBottomTabNavigator} from 'react-navigation-tabs';
 import SingIn from '~/pages/SingIn';
 import SingUp from '~/pages/SingUp';
 
-export default createAppContainer(
-  createSwitchNavigator({
-    SingIn,
-    SingUp,
-  }),
-);
+import Dashboard from './pages/Dashboard';
+
+export default (singedIn = false) =>
+  createAppContainer(
+    createSwitchNavigator(
+      {
+        Sing: createSwitchNavigator({
+          SingIn,
+          SingUp,
+        }),
+        App: createBottomTabNavigator({
+          Dashboard,
+        }),
+      },
+      {
+        initialRouteName: singedIn ? 'App' : 'Sing',
+      },
+    ),
+  );
