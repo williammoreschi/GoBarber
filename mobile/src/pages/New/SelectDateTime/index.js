52eabf56f7fd4c97625aba9536322fa9528add42
william moreschi <williammoreschi@gmail.com>
2020-04-17 21:39:01 -0300

    Mostrando os prestadores

--- mobile/src/pages/New/SelectDateTime/index.js
+++ mobile/src/pages/New/SelectDateTime/index.js
@@ -1,8 +1,24 @@
 import React from 'react';
-import {View} from 'react-native';
+import {TouchableOpacity} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import Background from '~/components/Background';
 
 // import { Container } from './styles';
 
-export default function SelectDateTime() {
-  return <View />;
+export default function SelectDateTime({navigation}) {
+  return <Background />;
 }
+
+SelectDateTime.navigationOptions = ({navigation}) => ({
+  title: 'Selecione um Horário',
+  headerTitleAlign: 'center',
+  headerLeft: () => (
+    <TouchableOpacity
+      onPress={() => {
+        navigation.navigate('SelectProvider');
+      }}>
+      <Icon name="chevron-left" size={20} color="#fff" />
+    </TouchableOpacity>
+  ),
+});
