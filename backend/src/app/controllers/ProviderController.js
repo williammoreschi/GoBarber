071b306aa93473d73a6ddf157e107cc56416b4f1
william moreschi <williammoreschi@gmail.com>
2020-04-28 21:03:02 -0300

    Criando um cache para lista de prestadores, esse cache é deletata todas vez que cria um novo prestador

--- backend/src/app/controllers/ProviderController.js
+++ backend/src/app/controllers/ProviderController.js
@@ -1,8 +1,15 @@
 import User from '../models/User';
 import File from '../models/File';
 
+import Cache from '../../lib/Cache';
+
 class ProviderController {
   async index(req, res) {
+    // Verifica se a lista de prestadores já está salva em cache
+    const cached = await Cache.get('providers');
+    if (cached) {
+      return res.json(cached);
+    }
     const providers = await User.findAll({
       attributes: ['id', 'name', 'email', 'avatar_id'],
       where: { provider: true },
@@ -14,6 +21,9 @@ class ProviderController {
         },
       ],
     });
+
+    // Cria um cache da lista de prestadores
+    await Cache.set('providers', providers);
 
     return res.json(providers);
   }
